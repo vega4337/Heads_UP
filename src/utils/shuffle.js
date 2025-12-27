d991f1d3aef1d9e3c1089facd903aa221320fddc
vega4337 <vega109@msn.com>
2025-12-27 16:49:46 -0600

    Fix category prompts + stabilize tilt logging

--- src/utils/shuffle.js
+++ src/utils/shuffle.js
@@ -1,8 +1,13 @@
-export function shuffle(array) {
-  const a = [...array];
-  for (let i = a.length - 1; i > 0; i--) {
+// src/utils/shuffle.js
+
+export function shuffleArray(input) {
+  const arr = Array.isArray(input) ? [...input] : [];
+  for (let i = arr.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    [a[i], a[j]] = [a[j], a[i]];
+    [arr[i], arr[j]] = [arr[j], arr[i]];
   }
-  return a;
+  return arr;
 }
+
+// Also provide a default export, in case any file imports it as default.
+export default shuffleArray;
